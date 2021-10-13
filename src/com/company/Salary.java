0afb58b189934efe3851603b778f8a2715dfdedb
Jelena <jelenbol@gmail.com>
2021-10-13 20:54:46 +0300

    Added tasks

--- src/com/company/Salary.java
+++ src/com/company/Salary.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class Salary {
     public static void main(String[] args) {
-        Scanner salary = new Scanner(System.in);
+     //   Scanner salary = new Scanner(System.in);
 
-            int workingHoursInDay = salary.nextInt();
+       //     int workingHoursInDay = salary.nextInt();
 
-        if (workingHoursInDay <= 8 && workingHoursInDay >0) {
-            System.out.println("Your rate is " + workingHoursInDay * 10 "eur")}
-         else (workingHoursInDay > 8 && workingHoursInDay <=24) {
-            System.out.println("Your rate is " + (80 + (workingHoursInDay -8 )* 15) + "eur"));}
+      //  if (workingHoursInDay <= 8 && workingHoursInDay >0) {
+      //      System.out.println("Your rate is " + workingHoursInDay * 10 "eur")}
+      //   else (workingHoursInDay > 8 && workingHoursInDay <=24) {
+      //      System.out.println("Your rate is " + (80 + (workingHoursInDay -8 )* 15) + "eur"));}
 
 
 
